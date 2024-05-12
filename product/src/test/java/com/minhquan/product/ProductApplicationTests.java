078f07c1c8e3bac45b8306aafc9010cb53d8eff0
minhquandoan <dtmquan2212@gmail.com>
2024-05-12 22:29:25 +0700

    [product-service] - Initial Commit

---
+++ product/src/test/java/com/minhquan/product/ProductApplicationTests.java
@@ -0,0 +1,14 @@
+package com.minhquan.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// 	System.out.println("Hello");
+	// }
+
+}
